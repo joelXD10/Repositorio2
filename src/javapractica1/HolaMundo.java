923fbbbd0a789e9a9d6ffc2c53ea1c7883fa1e1a
unknown <joelreinoso86@gmail.com>
2022-07-19 21:28:05 -0300

    Segundo Commit

---
+++ src/javapractica1/HolaMundo.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * kak jaiansn ajs ajnsjansjasjnasnkajsnakjsnkanand open the template in the editor.
+ */
+package javapractica1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author joel
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //EJERCICIO 3
+       /*int num1=7;
+       int num2=7;
+       int suma= num1+num2;
+       double division= num1/num2;
+       int multiplicacion= num1*num1;
+       System.out.println("El resultado es: "+ suma);
+       System.out.println("El resultado es: "+division);
+       System.out.println("El resultado es: "+multiplicacion);
+       System.out.print("Hola ");
+       System.out.print("Perro");
+       
+       ///EJERCICIO4
+       String nombre="joel";
+       int edad=25;
+       System.out.print(nombre+ " "+ edad);*/
+       
+      /* Scanner in= new Scanner (System.in);
+       double num ;
+       char caracter;
+       boolean log;
+       System.out.println("Ingrese un numero con 2 decimales");
+       num= in.nextDouble();
+       System.out.println("Ingrese una letra");
+       caracter = in.next().charAt(0);
+       System.out.println("Ingrese un valor Verdadero(true) o Falso(false)");
+       log = in.nextBoolean();
+       
+       ///Codigo donde se muetra los valores en pantalla
+       System.out.println("El numero elegido es: "+num);
+       System.out.println("El caracter elegido es: "+ caracter);
+       System.out.println("El valor elegido es: "+log); */
+      
+       //EJERCICIO DE PRUEBA
+      /*Scanner in= new Scanner (System.in);
+      int num1;
+      int num2;
+      int result;
+            System.out.println("Ingrese un numero");
+        num1= in.nextInt();
+            System.out.println("Ingrese otro numero");
+        num2= in.nextInt();
+      result=num1+num2;
+            if(result>10){;
+                System.out.println("Sos un crack: "+result);
+             } else{
+                System.out.println("Sos un huevon: "+result);
+                }*/
+    }      
+}
